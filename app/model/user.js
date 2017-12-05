138a4e0091369692bce90eda4f2f3dabad2abd32
alex klitman <alexklit@gmail.com>
2017-12-05 21:41:49 +0200

    creating a user constractor

---
+++ app/model/user.js
@@ -0,0 +1,24 @@
+
+landApp.factory("User", function() {
+    
+    // User Constructor
+    function Land(usertype, workerid, firstname, lastname, id, telephone, email, streetname,
+        housenumber, city)
+        {
+        this.usertype = usertype;
+        this.workerid = workerid;
+        this.propertynum = propertynum;
+        this.firstname = firstname;
+
+        this.lastname = lastname;
+        this.id = id;
+        this.telephone = telephone;
+        this.email = email;
+
+        this.streetname = streetname;
+        this.housenumber = housenumber;
+        this.city = city;
+    }
+    
+    return User; 
+  })
