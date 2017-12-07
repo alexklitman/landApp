3ee15171a0c00e13b754be8c1a3028c284ab0fda
alex klitman <alexklit@gmail.com>
2017-12-07 12:20:51 +0200

    updating google charts

--- app/statistics/chartsCtrl.js
+++ app/statistics/chartsCtrl.js
@@ -1,26 +1,44 @@
-landApp.controller("chartsCtrl", function ($scope, lands, $log) {
+landApp.controller("chartsCtrl", function ($scope, lands, $log, $http) {
     $scope.myChartObject = {};
 
     $scope.myChartObject.type = "PieChart";
 
-    lands.getAll().then(function mySuccess(data) {
+    //if the length of array lands is 0 meening never was loaded
+    if (lands.getAll().length === 0) {
+        // http call to get data from jso
+        $http.get("app/model/data/lands.json").then(function mySuccess(response) {
+            // Updating the service with the data
+            lands.setLands(response.data);
+
+            // Getting the data from the service
+            $scope.lands = lands.getAll();
+            loadChartData();
+        }, function myError(response) {
+            alert("error" + JSON.stringify(response.status));
+        });
+    } else {
+        $scope.lands = lands.getAll();
+        loadChartData();
+    }
+
+    function loadChartData() {
         var typeLandsBiulding = 0;
         var typeLandsApartment = 0;
         var typeLandsPlot = 0;
-        for (var i = 0; i < data.length; i++) {
+        for (var i = 0; i < $scope.lands.length; i++) {
             var currentLand = lands.getLandById(i);
             if (currentLand.buildingtype === "מבנה") {
                 typeLandsBiulding++;
             }
             if (currentLand.buildingtype === "דירה") {
-                
+                typeLandsApartment++;
             }
              else {
                 typeLandsPlot++;
             }
         }
 
-        $scope.myChartObject.data = {
+        $scope.myChartObject.lands = {
             "cols": [
                 { id: "t", label: "New or Old", type: "string" },
                 { id: "s", label: "Number of Cars", type: "number" }
@@ -44,14 +62,15 @@ landApp.controller("chartsCtrl", function ($scope, lands, $log) {
                     ]
                 }
             ]
-        };        
-    }, function myError(responseError) {
-        alert(JSON.stringify(responseError));
-    });
+        };    
 
+    }
 
     //Pie Chart Title
     $scope.myChartObject.options = {
         'title': 'התפלגות נכסים'
     };
+
+
+
 });
